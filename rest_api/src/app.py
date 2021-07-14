560567a54b4864bbbf4c25f8561db98a4ce34577
git <git@zero.localnet.jp>
2021-07-14 20:55:18 +0900

    add docker environment files

---
+++ rest_api/src/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, request
+from time import sleep
+import numpy as np
+from pyroombaadapter import PyRoombaAdapter
+import os
+
+# create app
+app = Flask('REST API')
+# create adapter
+adapter = PyRoombaAdapter('/dev/ttyUSB0')
+
+@app.route('/', method=['GET'])
+def get_command():
+    return 200
+
+if __name__ == '__main__':
+    port = os.getenv('SERVER_PORT', 10080)
+    app.run(port=port, debug=True)
