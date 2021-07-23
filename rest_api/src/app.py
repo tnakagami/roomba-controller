8f2a95a8951ba38904efb58ba93c1e9054fc093a
git <git@zero.localnet.jp>
2021-07-23 11:48:36 +0900

    docker-compose.ymlとapp.pyを更新

--- rest_api/src/app.py
+++ rest_api/src/app.py
@@ -46,4 +46,5 @@ def execute_command():
 
 if __name__ == '__main__':
     port = os.getenv('SERVER_PORT', 10080)
-    app.run(host='0.0.0.0', port=port, debug=True)
+    debug = True if os.getenv('DEBUG', 'false').lower() == 'true' else False
+    app.run(host='0.0.0.0', port=port, debug=debug)
